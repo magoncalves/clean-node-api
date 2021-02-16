54873ea57ac73787c984d701722ab4d44ccf7365
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-02-16 11:23:24 -0300

    feat: ensure requests return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,8 +3,8 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   it('should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
-      res.send(req.body)
+    app.get('/test_cors', (req, res) => {
+      res.send()
     })
 
     await request(app)
