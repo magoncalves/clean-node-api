8e7303d18710123323aa18fb9dda277a8a343913
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-02-17 09:41:28 -0300

    feat: ensure mongodb reconnects if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -15,7 +15,7 @@ describe('Account Mondo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
