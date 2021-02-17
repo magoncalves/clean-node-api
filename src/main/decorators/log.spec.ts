a6d41cbe6ae158a55359192a5a9b5bff703dee88
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-02-17 17:28:50 -0300

    feat: ensure LogControllerDecorator returns the same result of the Controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -53,4 +53,24 @@ describe('LogController Decorator', () => {
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  it('should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Murillo'
+      }
+    })
+  })
 })
