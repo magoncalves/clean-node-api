67c76594a6fbda3ee7c1022b9fd8b6d180dab6b9
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-02-27 22:13:10 -0300

    refactor: rename update to updateAccessToken

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -7,9 +7,11 @@ jest.mock('jsonwebtoken', () => ({
   }
 }))
 
+const makeSut = (): JwtAdapter => new JwtAdapter('secret')
+
 describe('JWT Adapter', () => {
   it('should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
 
@@ -17,9 +19,19 @@ describe('JWT Adapter', () => {
   })
 
   it('should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
 
     expect(accessToken).toEqual('any_token')
+  })
+
+  it('should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const encryptPromise = sut.encrypt('any_id')
+
+    await expect(encryptPromise).rejects.toThrow()
   })
 })
