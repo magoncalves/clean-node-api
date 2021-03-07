cc8d0d2d46c967a173541e4a59e03666b9e978fd
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-03-06 23:04:11 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/use-cases/add-survey/db-add-survey-factory.ts
@@ -0,0 +1,10 @@
+import { DbAddSurvey } from '../../../../data/use-cases/add-survey/db-add-survey'
+import { AddSurvey } from '../../../../domain/use-cases/add-survey'
+import { SurveyMongoRepository } from '../../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export const makeDbAddSurvey = (): AddSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  const dbAddSurvey = new DbAddSurvey(surveyMongoRepository)
+
+  return dbAddSurvey
+}
