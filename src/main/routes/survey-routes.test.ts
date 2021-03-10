6b07d72644d13f8230c421967a0a8311e09ba346
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-03-10 12:05:53 -0300

    feat: ensure add survey route returns 204 if a valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,10 +1,14 @@
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
 
 describe('Survey Routes', () => {
   let surveyCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL ?? '')
@@ -17,6 +21,9 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /survey', () => {
@@ -36,6 +43,46 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    it('should return 204 on add survey with valid access token', async () => {
+      const password = await hash('123', 12)
+      const res = await accountCollection.insertOne({
+        name: 'Murillo',
+        email: 'murillogoncalves@gmail.com',
+        password,
+        role: 'admin'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne(
+        {
+          _id: id
+        },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      )
+
+      await request(app)
+        .post('/api/survey')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'https://image-name.com',
+              answer: 'Answer 1'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
