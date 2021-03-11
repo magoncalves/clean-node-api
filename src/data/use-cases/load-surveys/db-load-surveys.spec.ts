44869dd6eb98281edd03b4d26178bee8210bfce1
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-03-10 22:55:16 -0300

    feat: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/use-cases/load-surveys/db-load-surveys.spec.ts
+++ src/data/use-cases/load-surveys/db-load-surveys.spec.ts
@@ -61,4 +61,12 @@ describe('DbLoadSurveys', () => {
 
     expect(loadAllSpy).toHaveBeenCalled()
   })
+
+  it('should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.load()
+
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
