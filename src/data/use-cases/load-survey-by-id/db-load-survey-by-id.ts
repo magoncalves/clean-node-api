ff012482aed28e2c0c996a629efdd85d18054a0f
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-03-14 23:07:06 -0300

    feat: ensure DbLoadSurveyById returns a survey on success

---
+++ src/data/use-cases/load-survey-by-id/db-load-survey-by-id.ts
@@ -0,0 +1,15 @@
+import { LoadSurveyById } from '@/domain/use-cases/load-survey-by-id'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor (
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const survey = await this.loadSurveyByIdRepository.loadById(id)
+
+    return survey
+  }
+}
