7b86cd0e42511f594c115139ce291276700cf83e
Murillo Azambuja Gonçalves <murillo.goncalves@thoughtworks.com>
2021-03-19 14:58:19 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,28 @@
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
